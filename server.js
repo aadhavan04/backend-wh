446b10896aa1097477222b39509c5286f802e188
aadhavan04 <aadhavan142@gmail.com>
2026-05-01 20:52:47 +0530

    removed env file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const workoutRoutes = require("./routes/workout");
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
+
 const app = express();
 
 app.use(cors());
